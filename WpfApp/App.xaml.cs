aee82c83bd367b473df7a8552f44202ad91b56e8
Hans Løken <hans.loeken@cgm.com>
2021-08-09 16:40:15 +0200

    WpfApp

---
+++ WpfApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var mainWindow = new MainWindow();
+            var viewModel = new MainWindowViewModel();
+            viewModel.Text1 = "Rupert";
+            viewModel.Text2 = "Banana";
+            mainWindow.DataContext = viewModel;
+            mainWindow.Show();
+        }
+    }
+}
---
+++ WpfApp/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _text1;
+        private string _text2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Text1
+        {
+            get => _text1;
+            set
+            {
+                _text1 = value;
+                OnPropertyChanged(nameof(Text1));
+            }
+        }
+
+        public string Text2
+        {
+            get => _text2;
+            set
+            {
+                _text2 = value;
+                OnPropertyChanged(nameof(Text2));
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
